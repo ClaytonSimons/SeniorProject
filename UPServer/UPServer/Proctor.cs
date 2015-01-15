d0e8063fc751942b7362e28eae3f3788be6da121
Clay <clayton.simons90@gmail.com>
2015-01-14 17:10:03 -0800

    UPServer

---
+++ UPServer/UPServer/Proctor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPServer
+{
+    class Proctor
+    {
+        private ServerConnectionManager SCM;
+        private UserData Data;
+        public void Proctor()
+        {
+            SCM = new ServerConnectionManager();
+            Data = new UserData();
+        }
+    }
+}
---
+++ UPServer/UPServer/ServerConnectionManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPServer
+{
+    class ServerConnectionManager
+    {
+        private List<String> clients;
+        public ServerConnectionManager()
+        {
+
+        }
+    }
+}
