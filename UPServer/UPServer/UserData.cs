1727e85bc678e3ad6df19578a94ddf9d1cf78ff3
Clay <clayton.simons90@gmail.com>
2015-01-29 21:10:22 -0800

    Starting database connection

--- UPServer/UPServer/UserData.cs
+++ UPServer/UPServer/UserData.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace UPServer
 {
-    class UserData
+    public class UserData
     {
+        public bool SaveData(UserClient client, List<KeyData.KeyEntry> data)
+        {
+            SqlConnection connection = new SqlConnection();
+            return true;
+        }
     }
+
 }
