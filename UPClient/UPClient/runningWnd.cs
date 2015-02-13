1a39dfa9373fa32eed676d3f2b582ee9c8d52952
Clay <clayton.simons90@gmail.com>
2015-02-13 11:32:44 -0800

    Connects to Worker

--- UPClient/UPClient/runningWnd.cs
+++ UPClient/UPClient/runningWnd.cs
@@ -19,7 +19,7 @@ namespace UPClient
             InitializeComponent();
             parent = p;
             //Need to retrieve serverAddress from an external txt file or something.
-            client = new Client("localhost", this);
+            client = new Client("WORKER", this);
         }
         public void updateError(String msg)
         {
